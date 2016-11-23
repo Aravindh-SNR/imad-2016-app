2362150435a099df358792dccf9d1979c8a46acd
Aravindh S N R <aravindhsnr@gmail.com>
2016-11-23 23:25:58 +0530

    Updated server.js

--- server.js
+++ server.js
@@ -5,12 +5,23 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+app.use(require('./routes/league.js'));
+app.use(require('./routes/squad.js'));
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+});
+
+app.set('options', {
+    hostname: 'api.football-data.org',
+    headers: { 'X-Auth-Token': '9f20857b262f461680629b9aa867166f' },
+    //Get your API key from api.football-data.org/register
+    //Or, comment out headers (not recommended)
+    method: 'GET'
 });
 
 var port = 8080;
