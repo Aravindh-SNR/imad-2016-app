cf393b379aaca467b7c1caa27f4fc4854675f2a7
Aravindh S N R <aravindhsnr@gmail.com>
2016-11-28 02:07:38 +0530

    Updated server.js

--- server.js
+++ server.js
@@ -2,9 +2,7 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
-var crypto = require('crypto');
 var bodyParser = require('body-parser');
-var session = require('express-session');
 
 var config = {
     user: 'aravindh-snr',
@@ -17,10 +15,6 @@ var config = {
 var app = express();
 app.use(morgan('combined'));
 app.use(bodyParser.json());
-app.use(session({
-    secret: 'someRandomSecretValue',
-    cookie: { maxAge: 1000 * 60 * 60 * 24 * 30}
-}));
 
 var app = express();
 app.use(morgan('combined'));
@@ -37,9 +31,9 @@ app.get('/ui/banner.jpg', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'banner.jpg'));
 });
 
-/*app.get('/feedback', function (req, res) {
+app.get('/feedback', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'feedback.html'));
-});*/
+});
 
 app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
@@ -47,71 +41,26 @@ app.get('/ui/main.js', function (req, res) {
 
 var pool = new Pool(config);
 
-app.get('/check-login', function (req, res) {
-   if (req.session && req.session.auth && req.session.auth.userId) {
-       // Load the user object
-       pool.query('SELECT * FROM "user" WHERE id = $1', [req.session.auth.userId], function (err, result) {
-           if (err) {
-              res.status(500).send(err.toString());
-           } else {
-              res.status(200).send(result.rows[0].username);    
-           }
-       });
-   } else {
-       res.status(200).send('Not logged in');
-   }
-});
-
-app.post('/login', function (req, res) {
-   var username = req.body.username;
-   var password = req.body.password;
-   
-   pool.query('SELECT * FROM "user" WHERE username = $1', [username], function (err, result) {
+app.post('/add-feedback', function (req, res) {
+   var name = req.body.name;
+   var feedback = req.body.feedback;
+   pool.query('INSERT INTO "feedback" (feedback, name) VALUES ($1, $2)', [feedback, name], function (err, result) {
       if (err) {
           res.status(500).send(err.toString());
       } else {
-          if (result.rows.length === 0) {
-              res.status(403).send('username/password is invalid');
-          } else {
-              // Match the password
-              var dbString = result.rows[0].password;
-              var salt = dbString.split('$')[2];
-              var hashedPassword = hash(password, salt); // Creating a hash based on the password submitted and the original salt
-              if (hashedPassword === dbString) {
-                
-                // Set the session
-                req.session.auth = {userId: result.rows[0].id};
-                // set cookie with a session id
-                // internally, on the server side, it maps the session id to an object
-                // { auth: {userId }}
-                
-                res.send('credentials correct!');
-                
-              } else {
-                res.status(403).send('username/password is invalid');
-              }
-          }
+          res.redirect('/get-feedback');
       }
    });
 });
 
-app.post('/create-user', function (req, res) {
-   var username = req.body.username;
-   var password = req.body.password;
-   var salt = crypto.randomBytes(128).toString('hex');
-   var dbString = hash(password, salt);
-   pool.query('INSERT INTO "user" (username, password) VALUES ($1, $2)', [username, dbString], function (err, result) {
-      if (err) {
-          res.status(500).send(err.toString());
-      } else {
-          res.send('User successfully created: ' + username);
-      }
-   });
-});
-
-app.get('/logout', function (req, res) {
-   delete req.session.auth;
-    res.status(200).send('user logged out successfully');
+app.get('/get-feedback', function (req, res) {
+   pool.query('SELECT feedback, name FROM "feedback"', function(err, result) {
+       if(err) {
+           res.status(500).send(err.toString());
+       } else {
+           res.send(JSON.stringify(result.rows));
+       }
+   }) 
 });
 
 var port = 8080;
