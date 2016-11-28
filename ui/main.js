35eba5a873ca02a86d4875a7984f3a95889f5282
Aravindh <aravindhsnr@gmail.com>
2016-11-28 22:06:32 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,56 +1,24 @@
-/*$(document).on('click', '#submit', function() {
-    var request = new XMLHttpRequest();
-    request.onreadystatechange = function () {
-        if (request.readyState === XMLHttpRequest.DONE){
-            if (request.status === 200) {
-                var feedbackContent = '';
-                var feedbackData = JSON.parse(this.responseText);
-                for(var i = feedbackData.length - 1; i >= 0; i--) {
-                    var feedbackItem = feedbackData[i];
-                    feedbackContent += `<p><b>${feedbackItem.name}</b></p>
-                                        <p>${feedbackItem.feedback}</p>
-                                        <hr>`;
-                }
-                document.getElementById('feedbackSection').innerHTML = feedbackContent;
-            }
-        }
+var submitFeedback = document.getElementById('submit');
+    submitFeedback.onclick = function () {
+        // Create a request object
+        var request = new XMLHttpRequest();
+        
+        // Capture the response and store it in a variable
+        request.onreadystatechange = function () {
+          if (request.readyState === XMLHttpRequest.DONE) {
+              // Take some action
+              if (request.status === 200) {
+                  alert('Feedback added successfully');
+              }
+          }
+        };
+        
+        // Make the request
+        var name = document.getElementById('username').value;
+        var feedback = document.getElementById('feedback').value;
+        console.log(username);
+        console.log(feedback);
+        request.open('POST', '/create-user', true);
+        request.setRequestHeader('Content-Type', 'application/json');
+        request.send(JSON.stringify({username: username, feedback: feedback})); 
     };
-    
-    var name = document.getElementById('name').value;
-    var feedback = document.getElementById('feedback').value;
-    request.open('POST', '/add-feedback', true);
-    request.setRequestHeader('Content-Type', 'application/json');
-    request.send(JSON.stringify({feedback: feedback, name: name}));
-});*/
-
-$("form").submit(function(e){
-    e.preventDefault();
-    var username = $('#name').val();
-    var feedback = $('#feedback').val();
-    console.log(username);
-    console.log(feedback);
-    $.ajax({
-        url: "/add-feedback",
-        async: true,
-        type: "POST",
-        contentType: 'application/json; charset=UTF-8',
-        data: JSON.stringify({feedback: feedback, username: username}),
-        success: function(data, status, xhr) {
-            console.log('hello3');
-            var feedbackContent = '';
-            var feedbackData = JSON.parse(data);
-            for(var i = feedbackData.length - 1; i >= 0; i--) {
-                var feedbackItem = feedbackData[i];
-                feedbackContent += `<p><b>${feedbackItem.username}</b></p>
-                                    <p>${feedbackItem.feedback}</p>
-                                    <hr>`;
-            }
-            $('#feedbackSection').innerHTML = feedbackContent;
-        },
-        error: function(xhr, status, err) {
-            console.log('hello4');
-            console.log(err.toString());
-            $('#feedbackSection').innerHTML = '';
-        }
-    });
-});
