f3692f89a0691f0776d316f71b340f1b5d59ad8e
Aravindh <aravindhsnr@gmail.com>
2016-11-28 21:48:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -52,14 +52,14 @@ app.post('/add-feedback', function (req, res) {
           res.status(500).send(err.toString());
       } else {
           /*res.redirect('/get-feedback');*/
-          pool.query('SELECT feedback, name FROM feedback', function(err, result) {
+          /*pool.query('SELECT feedback, name FROM feedback', function(err, result) {
               console.log('hello5');
                if(err) {
                    res.status(500).send(err.toString());
                } else {
                    res.send(JSON.stringify(result.rows));
                }
-   }) 
+        }) */
       }
    });
 });
